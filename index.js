793625b8f1a81e0bff12ace1da369120cd7e9a38
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-09-19 12:07:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+let hqBlockAddress = 42;
+//let destination;
+let dest;
+
+let distanceFromHqInBlocks = function(block){
+    return Math.abs(block-hqBlockAddress);
+}
+
+let distanceFromHqInFeet = function (block){
+  return  distanceFromHqInBlocks(block) * 264;
+    
+}
+
+let distanceTravelledInFeet = function(start,destination){
+    return Math.abs(destination-start) * 264;
+}
+
+let calculatesFarePrice = function(start, destination){
+    dest = distanceTravelledInFeet(start, destination)
+
+    if (dest<= 400) {
+        return 0
+    }else if (dest> 400 && dest<=2000) {
+        return( dest-400)*0.02
+    }else if (dest >2000 && dest<2500){
+        return 25
+    }else{
+        return "cannot travel that far"
+    }
+}
